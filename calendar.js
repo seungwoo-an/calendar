0591274b5cea9533cb295a58e65aadb0054ce17f
seungwoo <seungwoo.an93@gmail.com>
2020-11-08 19:54:08 +0900

    flip when date clicked

--- calendar.js
+++ calendar.js
@@ -6,7 +6,8 @@ const prev = document.getElementById("prev");
 const post = document.getElementById("post");
 const days = document.getElementsByClassName("days");
 const container = document.getElementById("container");
-
+const details = document.getElementById("details");
+const selectedDate = document.getElementById("selectedDate");
 const CURRENT_YEAR = today.getFullYear();
 const CURRENT_MONTH = today.getMonth() + 1;
 const CURRENT_DAY = dayOfWeek[today.getDay()];
@@ -17,50 +18,73 @@ let month = CURRENT_MONTH;
 let day = CURRENT_DAY;
 let date = CURRENT_DATE;
 
+let frontOrBack = true;
 console.log(`year : ${year}, month : ${month}, day : ${day}, date : ${date}`);
 
-function currentCheck(){
-    return (CURRENT_YEAR==year && CURRENT_MONTH==month)?true:false;
+function flip(event){
+    if(frontOrBack){
+        const clickedDate = event.target.innerText;
+        container.classList.add("flipped");
+        details.classList.remove("flipped");
+        showDetails(clickedDate);
+    }else{
+        container.classList.remove("flipped");
+        details.classList.add("flipped");
+    }
+    frontOrBack=!frontOrBack;
 }
-prev.addEventListener("click",function(){
+function showDetails(clickedDate) {
+    console.log(clickedDate);
+    selectedDate.innerText=`${year}년 ${month}월 ${clickedDate}일`
+}
+function currentCheck() {
+    return (CURRENT_YEAR == year && CURRENT_MONTH == month) ? true : false;
+}
+prev.addEventListener("click", function () {
     month--;
-    if(month<1){
-        month=12;
+    if (month < 1) {
+        month = 12;
         year--;
     }
-    if(currentCheck()){
+    if (currentCheck()) {
         date_day.classList.remove("hidden");
-    }else date_day.classList.add("hidden");
-
+        year_month.classList.remove("padded");
+    } else {
+        year_month.classList.add("padded");
+        date_day.classList.add("hidden");
+    }
     init();
 });
-post.addEventListener("click",function(){
+post.addEventListener("click", function () {
     month++;
-    if(month>12){
-        month=1;
+    if (month > 12) {
+        month = 1;
         year++;
     }
-    if(currentCheck()){
+    if (currentCheck()) {
         date_day.classList.remove("hidden");
-    }else date_day.classList.add("hidden");
+        year_month.classList.remove("padded");
+    } else {
+        year_month.classList.add("padded");
+        date_day.classList.add("hidden");
+    }
 
     init();
 });
-function showDetails(event){
-}
-function getStartDay(endDay,dates){
-    const startDay = (endDay-(dates%7-1)<0)? endDay-(dates%7-1)+6: endDay-(dates%7-1);
+
+function getStartDay(endDay, dates) {
+    const startDay = (endDay - (dates % 7 - 1) < 0) ? endDay - (dates % 7 - 1) + 6 : endDay - (dates % 7 - 1);
     return startDay;
 }
 function getDates(month) {
     switch (month) {
-        case 1:case 3: case 5: case 7: case 8: case 10:case 12:{
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12: {
             return 31;
         }
-        case 4: case 6: case 9:case 11: {
+        case 4: case 6: case 9: case 11: {
             return 30;
         }
-        default : {
+        default: {
             if (year % 4 == 0) {
                 if (year % 100 == 0) {
                     if (year % 400 == 0) {
@@ -79,28 +103,28 @@ function getDates(month) {
 }
 function setDates() {
     const dates = getDates(month);
-    const endDay = new Date(year,month-1,dates).getDay();
-    const startDay = getStartDay(endDay,dates);
-    
-    let dateCnt=1;
-    Array.from(days).forEach(day=>day.innerHTML="");
-    for(let i=startDay; i<dates+startDay;i++){
+    const endDay = new Date(year, month - 1, dates).getDay();
+    const startDay = getStartDay(endDay, dates);
+
+    let dateCnt = 1;
+    Array.from(days).forEach(day => day.innerHTML = "");
+    for (let i = startDay; i < dates + startDay; i++) {
         const link = document.createElement("a");
-        link.innerText=dateCnt;
-        link.addEventListener("click",showDetails);
+        link.innerText = dateCnt;
+        link.addEventListener("click", flip);
         let days_id = "";
-        if(currentCheck() && CURRENT_DATE==dateCnt){
-            days_id="currentDate";
-        }else{
-            days_id="";
+        if (currentCheck() && CURRENT_DATE == dateCnt) {
+            days_id = "currentDate";
+        } else {
+            days_id = "";
         }
-        days[i].id=days_id;
+        days[i].id = days_id;
         days[i].appendChild(link);
         dateCnt++;
     }
-    if(days[35].innerHTML==""){
+    if (days[35].innerHTML == "") {
         container.classList.add("shorten");
-    }else{
+    } else {
         container.classList.remove("shorten");
     }
 }
